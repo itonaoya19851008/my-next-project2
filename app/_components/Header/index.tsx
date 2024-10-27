840e172abdd3effcbe40b074935b5d6a0894f497
ito naoya <itonaoya1985@gmail.com>
2024-10-27 23:25:19 +0900

    update

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -15,6 +15,19 @@ export const Header = ()=>{
                     priority
                 />
             </Link>
+            <nav className={styles.nav}>
+                <ul className={styles.items}>
+                    <li>
+                        <Link href="/news">ニュース</Link>
+                    </li>
+                    <li>
+                        <Link href="/members">メンバー</Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">お問い合わせ</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./index.module.scss";
 import { FC } from "react";
 
@@ -19,24 +20,28 @@ export const NewsList:FC<Props> = (props)=>{
         <ul>
         {news.map((article)=>(
             <li className={styles.list} key={article.id}>
-              <div className={styles.link}>
-                <Image 
-                className={styles.image}
-                alt=""
-                src="/no-image.png"
-                width={1200}
-                height={630}
-                />
-                <dl className={styles.content}>
-                  <dt className="title">
-                    {article.title}
-                  </dt>
-                  <dd className={styles.meta}>
-                    <Cate category={article.category}/>
-                    <Date date={article.publishedAt}/>
-                  </dd>
-                </dl>
-              </div>
+              <Link href={`news/${article.id}`}
+              className={styles.link}
+              >
+                <div className={styles.link}>
+                  <Image
+                  className={styles.image}
+                  alt=""
+                  src="/no-image.png"
+                  width={1200}
+                  height={630}
+                  />
+                  <dl className={styles.content}>
+                    <dt className="title">
+                      {article.title}
+                    </dt>
+                    <dd className={styles.meta}>
+                      <Cate category={article.category}/>
+                      <Date date={article.publishedAt}/>
+                    </dd>
+                  </dl>
+                </div>
+              </Link>
             </li>
           ))}
         </ul>
